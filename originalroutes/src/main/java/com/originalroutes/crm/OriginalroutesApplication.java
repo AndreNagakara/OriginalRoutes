35b5eddc54a6cfa3d0a2d9bb984b40999a842de8
AndreNagakara <andre.nagakara@gmail.com>
2021-07-18 00:28:20 -0300

    Carga inicial

---
+++ originalroutes/src/main/java/com/originalroutes/crm/OriginalroutesApplication.java
@@ -0,0 +1,13 @@
+package com.originalroutes.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OriginalroutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OriginalroutesApplication.class, args);
+	}
+
+}
---
+++ originalroutes/src/test/java/com/originalroutes/crm/OriginalroutesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.originalroutes.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OriginalroutesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
